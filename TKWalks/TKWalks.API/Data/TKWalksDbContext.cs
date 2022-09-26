3def57b8dbc3e265806f66bd599d7a20e389b991
tanushka-21 <kanjilaltanushka2000@gmail.com>
2022-09-26 20:10:07 +0530

    END OF SECTION 2

---
+++ TKWalks/TKWalks.API/Data/TKWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TKWalks.API.Models.Domain;
+
+namespace TKWalks.API.Data
+{
+    public class TKWalksDbContext : DbContext
+    {
+        public TKWalksDbContext(DbContextOptions<TKWalksDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
--- TKWalks/TKWalks.API/Program.cs
+++ TKWalks/TKWalks.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using TKWalks.API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TKWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TKWalks"));
+});
 
 var app = builder.Build();
 
